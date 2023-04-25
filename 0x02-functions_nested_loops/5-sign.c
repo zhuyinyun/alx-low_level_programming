07fb31ad7fbe316cae41c5d7307d596e3574ab1c
zhuyinyun <chiri.bouchra@gmail.com>
2023-04-25 09:08:56 -0700

    Update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,14 +13,14 @@ if (n > 0)
 _putchar(43);
 return (1);
 }
-else if (n == 0)
-{
-_putchar(48);
-return (0);
-}
-else
+else if (n < 0)
 {
 _putchar(45);
 return (-1);
+}
+else
+{
+_putchar(48);
+return (0);
 }
 }
